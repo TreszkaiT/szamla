e213d2f8199d714c5e974f0e8566077e8a4d3592
TreszkaiT <treszkaitom@gmail.com>
2022-09-05 17:14:59 +0200

    project kialakítása

---
+++ src/main/java/com/tresz/szamla/SzamlaApplication.java
@@ -0,0 +1,13 @@
+package com.tresz.szamla;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SzamlaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SzamlaApplication.class, args);
+	}
+
+}
